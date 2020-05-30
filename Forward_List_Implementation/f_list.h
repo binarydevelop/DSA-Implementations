7dda209f6b56811b573c9de450498f75dedecb88
binarydevelop <binarydevelop@gmail.com>
2020-05-30 12:25:21 +0530

    printlist() and reverse list functions added

--- Forward_List_Implementation/f_list.h
+++ Forward_List_Implementation/f_list.h
@@ -1,3 +1,4 @@
+#include<iostream>
 #include"node.h"
 template<class T>
 class f_list{
@@ -17,11 +18,55 @@ class f_list{
        Node<T>* gettail(){return tail;}
 
        void addashead(T data){
-           Node<T>* new_node= new Node<T>(50);
+           Node<T>* new_node= new Node<T>(data);
            if(head==nullptr){
+               
                this->head= new_node;
                this->tail=new_node;
+               new_node->m_next=nullptr;
+               this->f_list_size+=1;
+           }else{
+               new_node->m_next=this->head;
+               this->head=new_node;
                this->f_list_size+=1;
            }
        }
-};
+
+       void addastail(T data){
+            Node<T>* new_node= new Node<T>(data);
+            if(tail==head){
+                new_node->m_next=nullptr;
+                this->tail=new_node;
+                this->f_list_size+=1;
+            }else{
+                this->tail->m_next=new_node;
+                new_node->m_next=nullptr;
+                this->tail=new_node;
+                this->f_list_size+=1;
+            }
+       }
+       
+       void printlist(){
+           Node<T>* temp =this->head;
+           while(temp){
+               std::cout<<temp->m_data<<"->";
+               temp=temp->m_next;
+           }
+       }
+
+       Node<T>* flistreverse(){
+           Node<T>* current= this->head;
+           Node<T>*  next=nullptr;
+           Node<T>* prev=nullptr;
+           while(current!=nullptr){
+               next=current->m_next;
+              current->m_next=prev;
+              prev=current;
+              current=next;
+           }
+           this->head=prev;
+           return prev;
+       }
+    
+       
+ };
--- Forward_List_Implementation/main.cpp
+++ Forward_List_Implementation/main.cpp
@@ -5,10 +5,23 @@ using namespace std;
 
 int main(){
     f_list<int> f1;
-    cout<<f1.gethead()<<" ";
+    cout<<f1.gethead()<<" Initail ";
 
     f1.addashead(60);
-    cout<<f1.gethead();
-    cout<<f1.gettail();
+ 
+    cout<<f1.gethead()<<"\n";
+
+    
+    f1.addashead(34);
+    cout<<f1.gethead()<<"\n";
+
+    cout<<f1.gettail()<<"\n";
+   
+    
+    f1.addastail(64);
+    f1.printlist();
+    cout<<" \n";
+    f1.flistreverse();
+    f1.printlist();
     return 0;
 }
