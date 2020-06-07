716f5aeb8d11d98b04bbd2783c34f64ddb5d9c7b
binarydevelop <binarydevelop@gmail.com>
2020-06-07 09:18:36 +0530

    Final Commit

--- Queue_Implementation_using_Linked_list/Queue.h
+++ Queue_Implementation_using_Linked_list/Queue.h
@@ -4,7 +4,51 @@
 template<class T>
 class Queue{
    private:
-      int length;
+      Node<T>* top;
+      Node<T>* rear;
+      int node_count;
    public:
+        T m_data;
+        Queue():node_count(0){
+           top=nullptr;
+           rear=nullptr;
+        }
+
+     int getnodecount(){return node_count;}
     
+     void enqueue(T value){
+       Node<T>* new_node= new Node<T>(value);
+       if(this->rear==nullptr){
+          this->top=new_node;
+          new_node->m_next=nullptr;
+          rear=new_node;
+          ++node_count;
+       }
+       else{
+          rear->m_next=new_node;
+          new_node->m_next=nullptr;
+          rear=new_node;
+          ++node_count;
+       }
+    }
+
+     void displayqueue(){
+        Node<T>* temp= this->top;
+        while(temp){
+           std::cout<<temp->m_data<<" ";
+           temp=temp->m_next;
+        }
+        std::cout<<"\n";
+     }
+     
+     void dequeue(){
+         if(top==nullptr){
+             return ;
+         }else{
+             Node<T>* temp=this->top;
+             top=top->m_next;
+             delete temp;
+             --node_count;
+         }
+     }
 };
--- Queue_Implementation_using_Linked_list/main.cpp
+++ Queue_Implementation_using_Linked_list/main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 int main(){
     
-    
-
-
+        Queue<int> q1;
+        q1.enqueue(5);
+        q1.enqueue(10);
+        q1.enqueue(15);
+        q1.enqueue(20);
+        cout<<q1.getnodecount();
+       q1.displayqueue();
+       q1.dequeue();
+       q1.dequeue();
+       cout<<q1.getnodecount();
+        q1.displayqueue();
     return 0;
 }
