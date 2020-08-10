58de04f75fd38e37e069be596508e498313aa782
binarydevelop <binarydevelop@gmail.com>
2020-08-10 09:46:08 +0530

    Deletion In Avl Tree

--- AVL_Tree_Implementation/avl.h
+++ AVL_Tree_Implementation/avl.h
@@ -1,4 +1,5 @@
 //UNDERSTAND - https://www.youtube.com/watch?v=a96JFhw5Ee4 [CODING Blocks]
+// Rob Edwards 
 #include"node.h"
 #include<queue>
 template<class T>
@@ -32,30 +33,47 @@ class avl{
     }
     //BALANCE_FACTOR
     int balance_factor(Node<T>* node){
+        if(node==nullptr){return 0;}
         return get_height(node->m_left)- get_height(node->m_right);
     }
-    Node<T>* ll_rotate(Node<T>* node){
-        Node<T>* temp=nullptr;
-        temp=node->m_right;
-        node->m_right=temp->m_left;
+    Node<T>* ll_rotate(Node<T>* &node){
+        Node<T>* temp=node->m_right;
+        Node<T>* orphan= temp->m_left;
+        node->m_right=orphan;
         temp->m_left=node;
         return temp;
     }
-     Node<T>* rr_rotate(Node<T>* node){
-        Node<T>* temp=nullptr;
-        temp=node->m_left;
-        node->m_left=temp->m_right;
+     Node<T>* rr_rotate(Node<T>* &node){
+        Node<T>* temp=node->m_left;
+        Node<T>* orphan =temp->m_right;
+        node->m_left=orphan;
         temp->m_right=node;
         return temp;
+
     }
-    Node<T>* rl_rotate(Node<T>* node){
+    Node<T>* rl_rotate(Node<T>* &node){
         node->m_right=rr_rotate(node->m_right);
         return ll_rotate(node);
     }
-        Node<T>* lr_rotate(Node<T>* node){
+        Node<T>* lr_rotate(Node<T>* &node){
         node->m_left=ll_rotate(node->m_left);
         return rr_rotate(node);
     }
+    //Rebalance Tree
+   Node<T>* rebalance(Node<T>* &node){
+       int bf= balance_factor(node);
+       if(bf>1){
+           if(balance_factor(node->m_left)>0){
+               return rr_rotate(node);
+           }else{return lr_rotate(node);}
+       }
+       if(bf<-1){
+           if(balance_factor(node->m_right)>0){
+               return rl_rotate(node);
+           }else{return ll_rotate(node);}
+       }
+       return node;
+   }
     //ADD_NODE
     Node<T>* add_node(T data,Node<T>* node){
             if(node==nullptr){
@@ -67,7 +85,7 @@ class avl{
             } else if(data<node->m_data){
                 node->m_left=add_node(data,node->m_left);
             }
-            int bf= balance_factor(node);
+        /*    int bf= balance_factor(node);
             //ll case
             if(bf>1 && data<node->m_left->m_data){
                 return rr_rotate(node);
@@ -80,8 +98,8 @@ class avl{
             }//rl case
              if(bf<-1 && data<node->m_right->m_data){
                   return rl_rotate(node);
-            }
-            return node;
+            }*/
+            return rebalance(node);
     }
     void add_node(T data){
        this->root=add_node(data,this->root);
@@ -142,7 +160,65 @@ class avl{
         node->m_right=in_order(node->m_right);
         return node;
     }
+     //findmin function
+     Node<T>* findmin(Node<T>* node){
+         if(node==nullptr){return node;}
+          else if(node->m_left==nullptr){
+                return node;
+            }
+         return findmin(node->m_left);
+     }
+     //findmax function
+     Node<T>* findmax(Node<T>* node){
+         if(node==nullptr){return node;}
+          else if(node->m_right==nullptr){
+                return node;
+            }
+         return findmax(node->m_right);
+     }
+
     //DELETE_NODE
+    void  delete_node(T data){
+        delete_node(data,this->root);
+    }
+    Node<T>* delete_node(T data,Node<T>* &node){
+        if(node==nullptr){return node;}
+        if(data<node->m_data){
+            node->m_left=delete_node(data,node->m_left);
+        }
+        if(data>node->m_data){
+            node->m_right=delete_node(data,node->m_right);
+        }
+        else if(data==node->m_data){//1st case
+              if(node->m_left==nullptr && node->m_right==nullptr){
+                   delete node;
+                  node=nullptr;
+                  
+                  //2nd case
+              }else if(node->m_left==nullptr){
+                  Node<T>* temp =node;
+                  node=node->m_right;
+                  delete temp;
+              }
+               else if(node->m_right==nullptr){
+                        Node<T>* temp=node;
+                        node=node->m_left;
+                        delete temp;
+                    }//3rd case
+                else {
+                    Node<T>* temp=findmin(node->m_right);
+                    node->m_data=temp->m_data;
+                    node->m_right=delete_node(temp->m_data,node->m_right);
+                
+                }
+        }
+        if(node){
+            node=rebalance(node);
+        }
+        
+        return node;
+    }
+    
     //SEARCH
     
 };
--- AVL_Tree_Implementation/main.cpp
+++ AVL_Tree_Implementation/main.cpp
@@ -9,17 +9,23 @@ int main(){
   a1.add_node(25);
   a1.add_node(30);  
   a1.add_node(10);
-  a1.add_node(5);
-  a1.add_node(15);
-  a1.add_node(27);
-    a1.add_node(19);
-      a1.add_node(16);
+ a1.add_node(5);
+ a1.add_node(15);
+ a1.add_node(27);
+ a1.add_node(19);
+ a1.add_node(16);
  
-a1.pre_order();
+  cout<<"\n";
+
+a1.level_order();
+
+
+
+a1.delete_node(19);
+a1.delete_node(16);
+
 cout<<"\n";
-a1.post_order();
-cout<<"\n";
-a1.in_order();
- 
+
+a1.level_order();
   
 }
